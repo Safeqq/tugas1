0c0fe34e24e53e3f3f5965d8a4a76274686a1c1a
unknown <syafiqahmadizzudin1@gmail.com>
2025-03-02 21:04:34 +0700

    chore: delete trash file

--- obj/Debug/net9.0/tugas1.AssemblyInfo.cs
+++ obj/Debug/net9.0/tugas1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tugas1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9056d7b711d59cdd1117e237b90d44abd191f63b")]
 [assembly: System.Reflection.AssemblyProductAttribute("tugas1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tugas1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
